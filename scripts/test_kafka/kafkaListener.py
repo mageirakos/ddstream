b077314fef4dc25309aa1a4a783877b90d44beba
mageirakos <up1053601@upnet.gr>
2022-04-17 17:44:15 +0300

    add kafka test scripts

---
+++ scripts/test_kafka/kafkaListener.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+kafka_topic_name = "weather"
+kafka_bootstrap_servers = "localhost:9092"
+
+if __name__ == "__main__":
+  ssc = SparkSession \
+          .builder \
+          .appName("test") \
+          .master("local[*]") \
+          .getOrCreate()
+
+  ssc.sparkContext.setLogLevel("ERROR")
+
+  weather_df = ssc \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic_name) \
+    .option("startingOffsets", "latest") \
+    .load()
+    
+  print("Printing Schema weather_df: ")
+  weather_df.printSchema()
+  
+  weather_df1 = weather_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "timestamp")
+
+  weather_write_stream = weather_df1 \
+      .writeStream \
+      .trigger(processingTime='5 seconds') \
+      .outputMode("update") \
+      .option("truncate", "false") \
+      .format("console") \
+      .start()
+
+  weather_write_stream.awaitTermination()
+  print("Stream Data Processing Application Completed.")
+  
