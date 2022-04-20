5a25da1d862911df037b38d54674d666b427a1ba
mageirakos <up1053601@upnet.gr>
2022-04-20 18:58:59 +0300

    blacked

--- scripts/test_kafka/kafka_spark_listener.py
+++ scripts/test_kafka/kafka_spark_listener.py
@@ -6,36 +6,33 @@ kafka_topic_name = "weather"
 kafka_bootstrap_servers = "localhost:9092"
 
 if __name__ == "__main__":
-  ssc = SparkSession \
-          .builder \
-          .appName("test") \
-          .master("local[*]") \
-          .getOrCreate()
+    ssc = SparkSession.builder.appName("test").master("local[*]").getOrCreate()
 
-  ssc.sparkContext.setLogLevel("ERROR")
+    ssc.sparkContext.setLogLevel("ERROR")
 
-  # Streaming Query
-  weather_df = ssc \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
-    .option("subscribe", kafka_topic_name) \
-    .option("startingOffsets", "latest") \
-    .load()
-    
-  print("Printing Schema weather_df: ")
-  weather_df.printSchema()
-  
-  weather_df1 = weather_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "timestamp")
+    # Streaming Query
+    weather_df = (
+        ssc.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers)
+        .option("subscribe", kafka_topic_name)
+        .option("startingOffsets", "latest")
+        .load()
+    )
 
-  weather_write_stream = weather_df1 \
-      .writeStream \
-      .trigger(processingTime='5 seconds') \
-      .outputMode("update") \
-      .option("truncate", "false") \
-      .format("console") \
-      .start()
+    print("Printing Schema weather_df: ")
+    weather_df.printSchema()
 
-  weather_write_stream.awaitTermination()
-  print("Stream Data Processing Application Completed.")
-  
+    weather_df1 = weather_df.selectExpr(
+        "CAST(key AS STRING)", "CAST(value AS STRING)", "timestamp"
+    )
+
+    weather_write_stream = (
+        weather_df1.writeStream.trigger(processingTime="5 seconds")
+        .outputMode("update")
+        .option("truncate", "false")
+        .format("console")
+        .start()
+    )
+
+    weather_write_stream.awaitTermination()
+    print("Stream Data Processing Application Completed.")
