a77eaa17e2991e0524dad8fc74d84ea3fe6a5073
mageirakos <up1053601@upnet.gr>
2022-04-22 17:42:22 +0300

    remove depr

--- ddstream/settings.py
+++ ddstream/settings.py
@@ -1,12 +1,2 @@
 DATA_PATH = "./data/"
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
-
-
-TEST_DF_SCHEMA = [
-    "split(value,',')[0] as col1",
-    "split(value,',')[1] as col2",
-    "split(value,',')[2] as col3",
-    "split(value,',')[3] as col4",
-    "split(value,',')[4] as col5",
-    "split(value,',')[4] as col6",
-]
