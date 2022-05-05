b25872d91e773c9a9566fcbf6f55a988ff382239
mageirakos <up1053601@upnet.gr>
2022-05-05 13:19:14 +0300

    update model.py

--- ddstream/model.py
+++ ddstream/model.py
@@ -1,26 +1,104 @@
-class DDStreamModel:
-    def __init__(self, broadcasted_var, ssc):
-        self.eps = 0.01
-        self.broadcasted_var = broadcasted_var
-        self.scc = ssc
+from microcluster import CoreMicroCluster
 
-    def assign_to_micro_cluster(self, row):
+# general
+import time
+
+
+class DDStreamModel:
+    def __init__(
+        self, epsilon=16.0, minPoints=10.0, beta=0.2, mu=10.0, lmbda=0.25, Tp=2
+    ):
+        self.eps = 0.01
+
+        self.time = 0
+        self.N = 0
+        self.currentN = 0
+
+        self.pMicroClusters = []  # array of microcluster objects
+        self.oMicroClusters = []
+
+        self.broadcastPMic = None
+        self.broadcastOMic = None
+
+        self.initialized = False
+        self.recursiveOutliersRMSDCheck = 1
+
+        self.initArr = []
+        self.tag = []
+
+        self.epsilon = epsilon
+        self.minPoints = minPoints
+        self.beta = beta
+        self.mu = mu
+        self.Tp = Tp
+        self.lmbda = lmbda
+
+        self.modelDetectPeriod = 0
+
+        self.lastEdit = 0
+        self.tfactor = 1.0
+
+        # idk
+        self.AlldriverTime = 0.0
+        self.AlldetectTime = 0.0
+        self.AllprocessTime = 0.0
+
+    def setCheck(self, t):
+        self.tfactor = t
+
+    def setTp(self):
+        # this.Tp = Math.round(1 / this.lambda * Math.log((this.beta * this.mu) / (this.beta * this.mu - 1))) + 1
+        pass
+
+    def initDBSCAN(self):
+        # initialize and broadcast pmc/omc
+        self.initialized = True
+
+    def getNeighborHood(self, pos, epsilon):
+        pass
+
+    def expandCluster(self):
+        pass
+
+    def run(self, df, batch_id):
+        """Run .foreachBatch()"""
+        # print(f"BATCH: {batch_id}", df, end="\n")
+        rdd = df.rdd.map(tuple)  # after this it is like for each rdd...
+        # this does not update the broadcasted_var but rather the local copy
+        # TODO: Should self.broad_var be global?
+
+        # print(f"BEFORE UPDATE: {self.broadcasted_var} {self.broadcasted_var.value}")
+        # self.broadcasted_var = rdd.context.broadcast((1,2,3,batch_id))
+        # print(f"AFTER UPDATE: {self.broadcasted_var} {self.broadcasted_var.value}")
+        # print()
+
+        lastEdit = 0
+        if not rdd.isEmpty() and self.initialized:
+            t0 = time.time()
+            print(f"The time is now {lastEdit}")
+
+            assignations = assignToMicroCluster(rdd, self.eps)
+
+    def assignToMicroCluster(self, row):
         if len(self.broadcasted_var.value) > 5:
             print("\n\nHERE\n\n")
         print(f"row assigned to micro cluster {row}")
         return (0, row)
 
-    def update_micro_clusters(self, assignations):
+    def assignToOutlierCluster(self):
+        pass
+
+    def computeDelta(self):
+        pass
+
+    def updateMicroClusters(self, assignations):
         print(f"update_mc {assignations}")
         pass
 
-    def run_batch(self, df, batch_id):
-        print(batch_id, df, end="\n")
-        self.broadcasted_var 
+    def ModelDetect(self):
+        pass
 
-    def run(self, row):
-        initialized = True
-        if row and initialized:
-            print(row)
-            assignations = self.assign_to_micro_cluster(row)
-            self.update_micro_clusters(assignations)
+    def FinalDetect(self):
+        pass
+
+    # TODO: add setters/getters anything I missed that might be used
